eb2dda10af22c2b9511ea74425bc1ac89e444c80
chi chun chen <chi.chun_chen844@icloud.com>
2014-08-26 06:03:30 +0800

    add user(not doing any change)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ActiveRecord::Base
 	mount_uploader :picture, PictureUploader
+
+	validates :name, :description, :presence => true
+  	validates :price, :numericality => { :greater_than_or_equal_to => 0.0 }
+  	validates :name, :uniqueness => true
 end
