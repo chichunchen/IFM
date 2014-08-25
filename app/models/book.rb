7c997f0c63a1aaeb661baacb1339d6d2404358cf
chi chun chen <chi.chun_chen844@icloud.com>
2014-08-26 01:09:58 +0800

    book bootstrap

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ActiveRecord::Base
+	validates :name, :description, :presence => true 
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.00} 
+	validates :name, :uniqueness => true
 end
