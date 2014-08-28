d1758ed253298bc3a0655b5b7731a992958396ef
chi chun chen <chi.chun_chen844@icloud.com>
2014-08-28 17:41:18 +0800

    books index modify

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,6 @@ class Book < ActiveRecord::Base
 
 	validates :name, :description, :presence => true
   	validates :price, :numericality => { :greater_than_or_equal_to => 0.0 }
-  	validates :name, :uniqueness => true
 
   	belongs_to :owner, :class_name => "User", :foreign_key => :user_id
 
