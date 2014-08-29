a131ac6edebedb363ea2a7db6b3d11dfdc286f00
chi chun chen <chi.chun_chen844@icloud.com>
2014-08-30 00:47:03 +0800

    fb send btn

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,10 +1,18 @@
 module BooksHelper
 	def order_mail book
-		user = User.find(book.user_id)
+		user = find_author(book)
 		mail_to( user.email,
-                 "寄信給 #{user.name}",
+                 image_tag("email-icon.png", :width => 30),
                  :subject => "資財二手書-#{book.name}",
                  :encode => "javascript" )
+	end
+
+	def find_author book
+		User.find(book.user_id)	
+	end
+
+	def order_fb
+		link_to( image_tag("fb-icon.jpg", :width => 32), '#' )
 	end
 
 	def same_author? book
