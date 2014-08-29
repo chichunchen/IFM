c9da78ade247f0a1972235b474747e87b5201099
chi chun chen <chi.chun_chen844@icloud.com>
2014-08-30 01:12:31 +0800

    fb plugin refine

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,21 +1,17 @@
 module BooksHelper
 	def order_mail book
-		user = find_author(book)
+		user = find_seller(book)
 		mail_to( user.email,
                  image_tag("email-icon.png", :width => 30),
                  :subject => "資財二手書-#{book.name}",
                  :encode => "javascript" )
 	end
 
-	def find_author book
+	def find_seller book
 		User.find(book.user_id)	
 	end
 
-	def order_fb
-		link_to( image_tag("fb-icon.jpg", :width => 32), '#' )
-	end
-
-	def same_author? book
+	def same_seller? book
 		current_user && book.editable_by?(current_user)	
 	end
 end
