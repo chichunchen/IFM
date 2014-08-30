c84c90d34995cee3b6b3a13d936d2efd41f64e83
chi chun chen <chi.chun_chen844@icloud.com>
2014-08-31 03:41:03 +0800

    will_paginate text customize

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ create_account = User.create([email: 'example@gmail.com',
 create_groups = for i in 1..20 do
                   Book.create!([name: "教科書 no.#{i}", 
                   				description: "這是用種子建立的第 #{i} 本教科書", 
-                  				picture: "email-icon.png", 
+                  				picture: "", 
                   				user_id: "1", 
                   				price: i * 100, 
                   				tag_list: "test, other"])
