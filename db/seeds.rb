100ee9b961d015c22327e079e0b6094b0ef12d7a
chi chun chen <chi.chun_chen844@icloud.com>
2014-09-01 02:16:19 +0800

    change tag form to single select

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,18 @@ create_account = User.create([email: 'example@gmail.com',
 							  password_confirmation: '12345678', 
 							  name: '測試用帳號'])
 
+tag_list = ["資訊", "財金", "外語", "通識", "印刷品", "其他"]
+create_tag = for i in 0..tag_list.length do
+                  ActsAsTaggableOn::Tag.create(:name => tag_list[i])
+             end
+
 create_groups = for i in 1..20 do
                   Book.create!([name: "教科書 no.#{i}", 
                   				description: "這是用種子建立的第 #{i} 本教科書", 
                   				picture: "fb-icon.jpg", 
                   				user_id: "1", 
                   				price: i * 100, 
-                  				tag_list: "測試"])
+                  				tag_list: tag_list[i % tag_list.length]
+                          ])
                 end
 
-tag_list = ["資訊", "財金", "外語", "通識", "印刷品", "其他", "測試"]
-create_tag = for i in 0..tag_list.length do
-                  ActsAsTaggableOn::Tag.create(:name => tag_list[i])
-             end
