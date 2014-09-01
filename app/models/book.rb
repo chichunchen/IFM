0a5b36d2e6e8bd3c35826fd86ee790260fe0e0ba
chi chun chen <chi.chun_chen844@icloud.com>
2014-09-02 02:30:54 +0800

    glyphicon

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,7 @@ class Book < ActiveRecord::Base
 
 	validates :name, :description, :presence => true
   	validates :price, :numericality => { :greater_than_or_equal_to => 0.0 }
-  	validates :legal, inclusion: { in: [true, false] }
-  	validates :original, inclusion: { in: [true, false] }
+  	validates :legal, :original, inclusion: { in: [true, false] }
   	# validates :picture, presence: true
   	
 
