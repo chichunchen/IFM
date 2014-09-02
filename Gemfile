153916c1f2a1cc2bf5bc2c8bd51bb633f9b9a1ea
chi chun chen <chi.chun_chen844@icloud.com>
2014-09-03 03:21:20 +0800

    heroku asset fix

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+ruby '2.1.2'
+
 gem 'bootstrap-sass'
 
 # gem 'mongoid', '3.0.4'
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,3 @@ create_tag = for i in 1..tag_list.length do
                   ActsAsTaggableOn::Tag.create(:name => tag_list[i])
              end
 
-create_books = for i in 1..20 do
-                  Book.create!([name: "教科書 no.#{i}", 
-                  				description: "這是用種子建立的第 #{i} 本教科書", 
-                  				picture: "fb-icon.jpg", 
-                  				user_id: "1", 
-                  				price: i * 100, 
-                  				tag_list: tag_list[i % tag_list.length]
-                          # legal: 
-                          # original: 
-                          ])
-                end
-
