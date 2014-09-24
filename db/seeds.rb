232206e0964cd7f752d4af9a409360c00b90d703
chi chun chen <chi.chun_chen844@icloud.com>
2014-09-24 21:29:45 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@ create_account = User.create([email: 'example@gmail.com',
 
 tag_list = ["資訊", "財金", "外語", "通識", "其他"]
 create_tag = for i in 1..tag_list.length do
-                  ActsAsTaggableOn::Tag.create(:name => tag_list[i])
+                ActsAsTaggableOn::Tag.create(:name => tag_list[i])
              end
 
+create_books = for i in 1..20 do
+					Book.create!([
+								name: "種子建立的第#{i}本書", 
+								description: "第#{i}個種子",		
+								price: "#{100*rand(15) + 100}",
+							 	legal: true,
+							 	original: true, 
+							 	user_id: "1", 
+							 	tag_list: "#{tag_list[rand(tag_list.length)]}"
+							 	#, picture:
+							])
+				end
